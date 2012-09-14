a6e9f1b4201d69e51bdf1da79c4816db53ec186d
Clint Floyd <admin@pinoydestination.com>
2012-09-14 14:27:26 +0800

    added frame

---
+++ pageframe.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+	<title>Pinoy Destination</title>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
+	<style>
+	html,body{
+		width:100%;
+		height:100%;
+		padding:0;
+		margin:0;
+		overflow:hidden;
+	}
+	iframe{
+		border:0;
+	}
+	</style>
+	<script>
+		$(document).ready(function(){
+			resizeFrame = function(){
+				var windowWidth = $(window).width();
+				var windowHeight = $(window).height();
+				console.log(windowWidth);
+				
+				$("#mainframe").width(windowWidth);
+				$("#mainframe").height(windowHeight);
+			};
+			
+			resizeFrame();
+			
+			$(window).resize(function(){
+				resizeFrame();
+			});
+			
+		});
+	</script>
+</head>
+<body>
+	<iframe id="mainframe" src="http://www.pinoydestination.com/"></iframe>
+</body>
+</html>
