281244325f43ed4ad90a87a144946e40e8d81b84
Clint <admin@pinoydestination.com>
2012-09-20 11:35:32 +0800

    adsenseadded

--- wp-content/themes/pinoydestination/sidebarboxad.php
+++ wp-content/themes/pinoydestination/sidebarboxad.php
@@ -1,13 +1,17 @@
 <div class="sidebarad300x250">
-	<script type="text/javascript"><!--
-		google_ad_client = "ca-pub-0908617034545427";
-		/* Pinoy Destination - Sidebar Ad */
-		google_ad_slot = "2317539732";
-		google_ad_width = 300;
-		google_ad_height = 250;
-		//-->
-	</script>
-	<script type="text/javascript"
-		    src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
-	</script>
+	<?php
+	$servername = $_SERVER["SERVER_NAME"];
+	if("www.pinoydestination.com" == $servername){
+	?>
+	 <!-- Begin BidVertiser code -->
+	<SCRIPT LANGUAGE="JavaScript1.1" SRC="http://bdv.bidvertiser.com/BidVertiser.dbm?pid=93354&bid=1212320" type="text/javascript"></SCRIPT>
+	<noscript><a href="http://www.bidvertiser.com/bdv/BidVertiser/bdv_publisher.dbm" rel="nofollow">make money</a></noscript>
+	<!-- End BidVertiser code --> 
+	<?php
+	}else{
+	?>
+	<img src="/images/ads/300x250.gif" alt="ads not found" />
+	<?php
+	}
+	?>
 </div>
