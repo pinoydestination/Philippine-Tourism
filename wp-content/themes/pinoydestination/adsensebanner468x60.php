4b3fb90da5f7cfee23751dcf31399214a8b676ef
Clint <admin@pinoydestination.com>
2012-09-21 08:49:03 +0800

    Files Updated. Added Google Adsense Code

--- wp-content/themes/pinoydestination/adsensebanner468x60.php
+++ wp-content/themes/pinoydestination/adsensebanner468x60.php
@@ -1,11 +1,24 @@
 <?php
 $servername = $_SERVER["SERVER_NAME"];
 if("www.pinoydestination.com" == $servername){
-?>
+/*
  <!-- Begin BidVertiser code -->
 <SCRIPT LANGUAGE="JavaScript1.1" SRC="http://bdv.bidvertiser.com/BidVertiser.dbm?pid=93354&bid=1212322" type="text/javascript"></SCRIPT>
 <noscript><a href="http://www.bidvertiser.com/bdv/BidVertiser/bdv_publisher.dbm" rel="nofollow">make money</a></noscript>
 <!-- End BidVertiser code --> 
+*/
+?>
+<script type="text/javascript"><!--
+google_ad_client = "ca-pub-0908617034545427";
+/* Pinoy Destination - small Ad Post */
+google_ad_slot = "9068492048";
+google_ad_width = 468;
+google_ad_height = 60;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
 <?php
 }else{
 ?>
--- wp-content/themes/pinoydestination/sidebarboxad.php
+++ wp-content/themes/pinoydestination/sidebarboxad.php
@@ -2,11 +2,24 @@
 	<?php
 	$servername = $_SERVER["SERVER_NAME"];
 	if("www.pinoydestination.com" == $servername){
-	?>
-	 <!-- Begin BidVertiser code -->
+	/* <!-- Begin BidVertiser code -->
 	<SCRIPT LANGUAGE="JavaScript1.1" SRC="http://bdv.bidvertiser.com/BidVertiser.dbm?pid=93354&bid=1212320" type="text/javascript"></SCRIPT>
 	<noscript><a href="http://www.bidvertiser.com/bdv/BidVertiser/bdv_publisher.dbm" rel="nofollow">make money</a></noscript>
-	<!-- End BidVertiser code --> 
+	<!-- End BidVertiser code --> */
+	?>
+	
+	<script type="text/javascript"><!--
+	google_ad_client = "ca-pub-0908617034545427";
+	/* Pinoy Destination - Sidebar Ad */
+	google_ad_slot = "2317539732";
+	google_ad_width = 300;
+	google_ad_height = 250;
+	//-->
+	</script>
+	<script type="text/javascript"
+	src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+	</script>
+	
 	<?php
 	}else{
 	?>
