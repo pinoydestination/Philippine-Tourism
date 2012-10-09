ceaeec971ae959324a996ec27b25a5fc2bda17ad
Clint <admin@pinoydestination.com>
2012-10-09 08:13:11 +0800

    static map updated

--- wp-content/themes/pinoydestination/single_sidebar.php
+++ wp-content/themes/pinoydestination/single_sidebar.php
@@ -7,7 +7,7 @@
 			<iframe width="300" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=+&amp;q=Philippines&amp;ie=UTF8&amp;hq=&amp;hnear=Philippines&amp;t=m&amp;ll=12.897489,121.816406&amp;spn=25.499947,26.279297&amp;z=4&amp;output=embed"></iframe>
 			<iframe width="300" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://www.pinoydestination.com/gmap.php?address=<?php echo $GLOBALS['Current_Location']; ?>&zoom=13"></iframe>
 			 * */ 
-			$additional = $GLOBALS['GoogleMap_Type'] != "" ? "&datatype=".$GLOBALS['GoogleMap_Type'] ? ""; 
+			$additional = $GLOBALS['GoogleMap_Type'] != "" ? "&datatype=".$GLOBALS['GoogleMap_Type'] : ""; 
 			?>
 			<img class="staticgmap" width="300" height="400" border="0" src="/googlemap/gstatic.php?address=<?php echo $GLOBALS['Current_Coordinates'].$additional; ?>&zoom=13&size=300x400" />
 		</div>
