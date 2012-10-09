69f9fbda00dff5775d9fdad333754abe5b8c9200
Clint <admin@pinoydestination.com>
2012-10-09 09:50:46 +0800

    added new gmap

--- googlemap/gmap.php
+++ googlemap/gmap.php
@@ -42,6 +42,59 @@ if(isset($_GET['zoom'])){
     <script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyAWo2NkY1CvmTYlKZRwS0P5ZfMSE5wLiiE&sensor=true"></script>
     <script src="/wp-content/themes/pinoydestination/js/jquery.js"></script>
 <script>
+
+	<?php 
+	if(isset($_GET['datatype']) && $_GET['datatype'] == "latlang"){
+	?>
+	$.noConflict();
+      var geocoder;
+      var map;
+      function initialize() {
+        geocoder = new google.maps.Geocoder();
+        var address = "<?php echo urldecode($_GET['address']); ?>";
+		
+		var parliament = new google.maps.LatLng(<?php echo urldecode($_GET['address']); ?>);
+		var marker;
+		var map;
+        
+       var latlng = new google.maps.LatLng(<?php echo urldecode($_GET['address']); ?>);
+        var mapOptions = {
+          zoom: <?php echo $zoomLevel; ?>,
+          mapTypeId: google.maps.MapTypeId.ROADMAP,
+		  center: latlng
+        }
+		
+		map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+		
+		marker = new google.maps.Marker({
+          map:map,
+          draggable:true,
+          animation: google.maps.Animation.DROP,
+          position: parliament
+        });
+        google.maps.event.addListener(marker, 'click', toggleBounce);
+		
+		
+        
+      }
+	  
+	  function toggleBounce() {
+
+        if (marker.getAnimation() != null) {
+          marker.setAnimation(null);
+        } else {
+          marker.setAnimation(google.maps.Animation.BOUNCE);
+        }
+      }
+      
+      jQuery(document).ready(function(){
+      	initialize();
+      });
+      
+	<?php
+	}else{
+	?>
+
 	 $.noConflict();
       var geocoder;
       var map;
@@ -69,28 +122,11 @@ if(isset($_GET['zoom'])){
         });
         map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
       }
-
-      function codeAddress() {
-        var address = "<?php echo $_GET['address']; ?>";
-        geocoder.geocode( { 'address': address}, function(results, status) {
-          if (status == google.maps.GeocoderStatus.OK) {
-            map.setCenter(results[0].geometry.location);
-            var marker = new google.maps.Marker({
-                map: map,
-                zoom:6,
-                position: results[0].geometry.location
-            });
-          } else {
-            alert('Geocode was not successful for the following reason: ' + status);
-          }
-        });
-      }
       
       jQuery(document).ready(function(){
       	initialize();
-      	//codeAddress()
       });
-      
+    <?php } ?>
     </script>
   </head>
   <body onload="">
--- wp-content/themes/pinoydestination/single_sidebar.php
+++ wp-content/themes/pinoydestination/single_sidebar.php
@@ -1,13 +1,15 @@
+<?php
+$additional = $GLOBALS['GoogleMap_Type'] != "" ? "&datatype=".$GLOBALS['GoogleMap_Type'] : ""; 
+?>
 <div class="right" id="sidebarPanel">
 	<div id="postsidebarcontainer">
 		<div class="sidebarmap">
-			<span class="myriad_pro_bold_condensed sidebarheader" style="float:left;"><?php echo get_the_title(); ?> Map</span><a class="largermapbutton fancybox fancybox.iframe" rel="nofollow" href="/googlemap/gmap.php?address=<?php echo urlencode($GLOBALS['Current_Coordinates']); ?>&zoom=13">ENLARGE MAP</a>
+			<span class="myriad_pro_bold_condensed sidebarheader" style="float:left;"><?php echo get_the_title(); ?> Map</span><a class="largermapbutton fancybox fancybox.iframe" rel="nofollow" href="/googlemap/gmap.php?address=<?php echo urldecode($GLOBALS['Current_Coordinates'].$additional); ?>&zoom=13">ENLARGE MAP</a>
 			<br clear="all" />
 			<?php /*
 			<iframe width="300" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=+&amp;q=Philippines&amp;ie=UTF8&amp;hq=&amp;hnear=Philippines&amp;t=m&amp;ll=12.897489,121.816406&amp;spn=25.499947,26.279297&amp;z=4&amp;output=embed"></iframe>
 			<iframe width="300" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://www.pinoydestination.com/gmap.php?address=<?php echo $GLOBALS['Current_Location']; ?>&zoom=13"></iframe>
 			 * */ 
-			$additional = $GLOBALS['GoogleMap_Type'] != "" ? "&datatype=".$GLOBALS['GoogleMap_Type'] : ""; 
 			?>
 			<img class="staticgmap" width="300" height="400" border="0" src="/googlemap/gstatic.php?address=<?php echo $GLOBALS['Current_Coordinates'].$additional; ?>&zoom=13&size=300x400" />
 		</div>
