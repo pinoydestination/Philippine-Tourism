a0cc562a4e530f337f8467e29561be466f81456a
Clint Floyd <admin@pinoydestination.com>
2012-10-30 10:20:32 +0800

    image updated

--- wp-content/themes/pinoydestination/heroshot.php
+++ wp-content/themes/pinoydestination/heroshot.php
@@ -1,13 +1,13 @@
 <div id="heroshot" class="heroshot">
 	<div class="heroshot_container">
 		<div>
-			<a href="http://www.pinoydestination.com/vacation/philippines/visayas/destination-visayas/boracay"><img src="/images/boracay-hero.jpg" border="0" alt="boracay island" /></a>
+			<a href="http://www.pinoydestination.com/vacation/philippines/visayas/destination-visayas/boracay"><img width="600" height="300" src="/images/boracay-hero.jpg" border="0" alt="boracay island" /></a>
 		</div>
 		<div>
-			<a href="http://www.pinoydestination.com/vacation/philippines/luzon/destination-luzon/bicol"><img src="/images/bicol-hero.jpg" border="0" alt="bicol island" /></a>
+			<a href="http://www.pinoydestination.com/vacation/philippines/luzon/destination-luzon/bicol"><img width="600" height="300" src="/images/bicol-hero.jpg" border="0" alt="bicol island" /></a>
 		</div>
 		<div>
-			<a href="http://www.pinoydestination.com/company/submit-review"><img src="/images/contribute-hero.jpg" border="0" alt="contribute" /></a>
+			<a href="http://www.pinoydestination.com/company/submit-review"><img width="600" height="300" src="/images/contribute-hero.jpg" border="0" alt="contribute" /></a>
 		</div>
 	</div>
 </div>
