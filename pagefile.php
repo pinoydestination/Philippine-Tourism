c88d8c8eade03a689b43ac1753887ee194a0485a
Clint Floyd <admin@pinoydestination.com>
2012-11-10 16:27:24 +0800

    page added.php

---
+++ pagefile.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>Pinoy Destination</title>
+	<style>
+		html,body{
+			margin:0;
+			padding:0;
+		}
+	</style>
+</head>
+<body>
+	<iframe src="http://www.pinoydestination.com/" width="300" height="300" ></iframe><noscript>iFrame not supported</noscript>
+</body>
+</html>
